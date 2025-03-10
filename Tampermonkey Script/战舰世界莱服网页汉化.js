4a0e42c61ba3cc41714eb25757e098dd8ad950af
windofxy <1594251995@qq.com>
2025-03-10 20:09:06 +0800

    Update Tampermonkey Script

---
+++ Tampermonkey Script/战舰世界莱服网页汉化.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         战舰世界莱服网页汉化
+// @namespace    https://github.com/windofxy
+// @version      202503051039
+// @description  战舰世界莱服网页汉化
+// @author       Windofxy
+// @icon         https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/icon128.png
+// @grant        none
+
+// @run-at       document_idle
+
+// @match        http://armory.korabli.su/*
+// @match        https://armory.korabli.su/*
+// @match        http://clans.korabli.su/*
+// @match        https://clans.korabli.su/*
+// @match        http://dockyard.korabli.su/*
+// @match        https://dockyard.korabli.su/*
+// @match        http://friends.korabli.su/*
+// @match        https://friends.korabli.su/*
+// @match        http://profile.korabli.su/*
+// @match        https://profile.korabli.su/*
+// @match        http://warehouse.korabli.su/*
+// @match        https://warehouse.korabli.su/*
+
+// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/Framework/framework.js
+// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-armory.js
+// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-clan.js
+// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-dockyard.js
+// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-friends.js
+// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-profile.js
+// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-warehouse.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your code here...
+})();
