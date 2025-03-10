c2a8f32c98e44d7ac81386adb9bf8f22f71ddb07
windofxy <1594251995@qq.com>
2025-03-11 00:13:27 +0800

    Tampermonkey Script Update 202503110012

--- Tampermonkey Script/战舰世界莱服网页汉化.js
+++ Tampermonkey Script/战舰世界莱服网页汉化.js
@@ -1,11 +1,15 @@
 // ==UserScript==
 // @name         战舰世界莱服网页汉化
 // @namespace    https://github.com/windofxy
-// @version      202503051039
-// @description  战舰世界莱服网页汉化
+// @version      202503110012
+// @description  一个将战舰世界莱服网页内容翻译为中文的插件
 // @author       Windofxy
-// @icon         https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/icon128.png
+// @icon         https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Tampermonkey%20Script/icon128.png
 // @grant        none
+// @license      MIT
+
+// @downloadURL  https://update.greasyfork.org/scripts/529417/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96.user.js
+// @updateURL    https://update.greasyfork.org/scripts/529417/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96.meta.js
 
 // @run-at       document_idle
 
@@ -22,13 +26,20 @@
 // @match        http://warehouse.korabli.su/*
 // @match        https://warehouse.korabli.su/*
 
-// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/Framework/framework.js
-// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-armory.js
-// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-clan.js
-// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-dockyard.js
-// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-friends.js
-// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-profile.js
-// @require      https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Chrome%20Extension/InGameBrowserLocalizer/localizer-warehouse.js
+// 代码-框架
+// @require      https://update.greasyfork.org/scripts/529413/1551061/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96-%E4%BB%A3%E7%A0%81-%E6%A1%86%E6%9E%B6.js
+// 资源-兵工厂翻译
+// @require      https://update.greasyfork.org/scripts/529395/1550983/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96-%E8%B5%84%E6%BA%90-%E5%85%B5%E5%B7%A5%E5%8E%82%E7%BF%BB%E8%AF%91.js
+// 资源-军团翻译
+// @require      https://update.greasyfork.org/scripts/529397/1550989/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96-%E8%B5%84%E6%BA%90-%E5%86%9B%E5%9B%A2%E7%BF%BB%E8%AF%91.js
+// 资源-造船厂翻译
+// @require      https://update.greasyfork.org/scripts/529398/1550991/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96-%E8%B5%84%E6%BA%90-%E9%80%A0%E8%88%B9%E5%8E%82%E7%BF%BB%E8%AF%91.js
+// 资源-征募站翻译
+// @require      https://update.greasyfork.org/scripts/529402/1551011/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96-%E8%B5%84%E6%BA%90-%E5%BE%81%E5%8B%9F%E7%AB%99%E7%BF%BB%E8%AF%91.js
+// 资源-账号档案翻译
+// @require      https://update.greasyfork.org/scripts/529405/1551022/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96-%E8%B5%84%E6%BA%90-%E8%B4%A6%E5%8F%B7%E6%A1%A3%E6%A1%88%E7%BF%BB%E8%AF%91.js
+// 资源-仓库翻译
+// @require      https://update.greasyfork.org/scripts/529406/1551020/%E6%88%98%E8%88%B0%E4%B8%96%E7%95%8C%E8%8E%B1%E6%9C%8D%E7%BD%91%E9%A1%B5%E6%B1%89%E5%8C%96-%E8%B5%84%E6%BA%90-%E4%BB%93%E5%BA%93%E7%BF%BB%E8%AF%91.js
 // ==/UserScript==
 
 (function() {
