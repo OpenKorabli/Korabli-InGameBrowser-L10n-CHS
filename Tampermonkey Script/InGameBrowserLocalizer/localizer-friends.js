d6e4326a4d93ca2495323c51f74dc906de410979
windofxy <1594251995@qq.com>
2025-03-10 21:32:00 +0800

    Update Tampermonkey Script

--- Tampermonkey Script/InGameBrowserLocalizer/localizer-friends.js
+++ Tampermonkey Script/InGameBrowserLocalizer/localizer-friends.js
@@ -2,7 +2,7 @@
 // @name         战舰世界莱服网页汉化-资源-征募站翻译
 // @namespace    https://github.com/windofxy
 // @version      202503051039
-// @description  战舰世界莱服网页汉化-资源-征募站翻译
+// @description  战舰世界莱服网页汉化-资源
 // @author       Windofxy
 // @icon         https://gh-proxy.net/github.com/OpenKorabli/Korabli-InGameBrowser-L10n-CHS/blob/main/Tampermonkey%20Script/icon128.png
 // @grant        none
